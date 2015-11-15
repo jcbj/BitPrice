c0c64383fac1b63d5e8514f190ff726d9e20bf5c
jiangchao <jiangchaobj@yahoo.com>
2015-11-15 16:39:36 +0800

    获取最近5天的历史价格

--- BitPrice/BitPrice/ViewController.swift
+++ BitPrice/BitPrice/ViewController.swift
@@ -21,6 +21,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     
         self.reloadPrice()
+        
+        self.buildHistoryLabels(self.getLastFiveDayPrice())
     }
 
     override func didReceiveMemoryWarning() {
@@ -65,6 +67,68 @@ class ViewController: UIViewController {
             return json["bpi"]["CNY"]["rate"].stringValue
         } else {
             return nil;
+        }
+    }
+    
+    /*
+    http://api.coindesk.com/v1/bpi/historical/close.json?start=2015-07-15&end=2015-07-24&currency=CNY
+    
+    {
+    "bpi": {
+    "2015-07-15": 1756.5732,
+    "2015-07-16": 1719.6188,
+    "2015-07-17": 1723.7974,
+    "2015-07-18": 1698.9991,
+    "2015-07-19": 1686.3934,
+    "2015-07-20": 1723.3102,
+    "2015-07-21": 1702.5693,
+    "2015-07-22": 1710.3503
+    },
+    "disclaimer": "This data was produced from the CoinDesk Bitcoin Price Index. BPI value data returned as CNY.",
+    "time": {
+    "updated": "Jul 23, 2015 09:53:17 UTC",
+    "updatedISO": "2015-07-23T09:53:17+00:00"
+    }
+    }
+
+    */
+    
+    func getLastFiveDayPrice() -> Array<(String, String)> {
+        let curDate = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let startDate = calendar.dateByAddingUnit(NSCalendarUnit.Day, value: -6, toDate: curDate, options: NSCalendarOptions.init(rawValue: 0))
+        let endDate = calendar.dateByAddingUnit(.Day, value: -1, toDate: curDate, options: NSCalendarOptions.init(rawValue: 0))
+        
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        
+        let url = "http://api.coindesk.com/v1/bpi/historical/close.json?start=\(formatter.stringFromDate(startDate!))&end=\(formatter.stringFromDate(endDate!))&currency=CNY"
+        
+        var result = Array<(String,String)>()
+        if let jsonData = NSData(contentsOfURL: NSURL(string: url)!) {
+            let json = JSON(data: jsonData)
+            let bpiDict:JSON = json["bpi"]
+            for (key,val) in bpiDict {
+                result.append((key,val.stringValue))
+            }
+        }
+        
+        return result
+    }
+    
+    func buildHistoryLabels (priceList: Array<(String, String)>) {
+        var count = 0.0
+        
+        let labelTitle = UILabel(frame: CGRectMake(CGFloat(30.0),CGFloat(250.0),CGFloat(200),CGFloat(30)))
+        labelTitle.text = "历史价格"
+        self.view.addSubview(labelTitle)
+        
+        for (date, price) in priceList {
+            let labelHistory = UILabel(frame: CGRectMake(CGFloat(30.0),CGFloat(280 + count * 40),CGFloat(200.0),CGFloat(30.0)))
+            labelHistory.text = "\(date) \(price)"
+            self.view.addSubview(labelHistory)
+            
+            count++
         }
     }
 
